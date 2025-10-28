a2ea38c1da74e2e62cda7a78bf0f49cf8c0958f3
Tadeo Gavensky <tadeogavensky@gmail.com>
2025-10-28 20:53:42 -0300

    neon db

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-import express from "express"
+import app from "./src/app.js";
+import "dotenv/config";
+import sequelize from "./src/database/connection.js";
 
-const app = express()
+const PORT = process.env.EXPRESS_PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto: ${PORT}`);
+});
+
+try {
+  await sequelize.authenticate();
+  console.log("Conexión establecida correctamente.");
+} catch (error) {
+  console.error("Error al conectar:", error);
+} finally {
+  await sequelize.close();
+}
