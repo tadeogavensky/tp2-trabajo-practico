7408724de120461fdc125e013f83686ae8cdf474
adrian.addesso <adrian.addesso@gmail.com>
2025-11-09 10:48:34 -0300

    Fixed routing issue on services/userServices.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,4 @@
-import { User } from "../models/index.js";
+import { User } from "../../models/index.js";
 import { comparePassword } from "../utils/user.js";
 class UserService {
   async getAllUsers() {
